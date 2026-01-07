9c8801f9c2b13bee28dfd749e1332fd2730f181f
M4SPIDER <vvvr8412@gmail.com>
2026-01-07 19:31:09 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import katex from 'katex';
+import 'katex/dist/katex.min.css';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -20,3 +22,4 @@ export default defineConfig({
     ]
   }
 });
+
