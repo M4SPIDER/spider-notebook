fbf8f9bd141b750dca815c80254326c32b984694
M4SPIDER <vvvr8412@gmail.com>
2026-05-28 15:07:38 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -108,7 +108,7 @@ const examples = [
     { id: 3, title: "Java Greeting", code: `java {\n  public class Main {\n    public static void main(String[] args) {\n      System.out.println("Java is ready!");\n    }\n  }\n}`, output: "Java is ready!" }
 ];
 
-const SPIDER_INSTALLER_URL = 'https://github.com/M4SPIDER/spider-notebook/releases/download/spider/M4.Spidernotebook.installer.exe';
+const SPIDER_INSTALLER_URL = 'https://github.com/M4SPIDER/spider-notebook/releases/download/spider/M4.SpidernotebookInstaller.exe';
 
 const initialTerminalOutput = [
     'Welcome to SpiderNoteBook!',
